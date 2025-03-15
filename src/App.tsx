b511cb767d7a6218c7d2c0fe4ab34f72bdee2d24
Marcin Mika <marcin.mika123@gmail.com>
2025-03-15 21:17:23 +0100

    Update project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CustomNavbar from './components/Navbar';
-import Home from './pages/Home';
-import Menu from './pages/Menu';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import Home from './pages/Home/Home';
+import Menu from './pages/Menu/Menu';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
 import './App.css';
 
 const App = () => {
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
+import './Home.css';
+
 const Home = () => {
   return (
-    <div className="container my-4">
+    <div className="container my-4 style">
       <h1 className="text-center">Welcome to Coffee Shop!</h1>
       <p className="text-center mt-3">The best coffee in town.</p>
     </div>
